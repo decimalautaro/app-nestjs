3f301d4c2fbaacc201e82a662b511a6a5f1c13c4
Lautaro Decima <decimalautaro@gmail.com>
2023-01-03 11:07:44 -0300

    agregando ejemplo en modulo users

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get('say-hello')
+    getHello(): string {
+      return this.usersService.getHello();
+    }
+}
