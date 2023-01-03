20dc5a1a15de9529f113ada7e7cd12bc03e81ff2
Lautaro Decima <decimalautaro@gmail.com>
2023-01-03 10:26:45 -0300

    feat: agregando controlador y service a users

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
 
-@Module({})
+@Module({
+  providers: [UsersService],
+  controllers: [UsersController]
+})
 export class UsersModule {}
