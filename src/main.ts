c803fa8dfda7b09775f34120b917016d55c95f88
Lautaro Decima <decimalautaro@gmail.com>
2023-01-04 10:31:30 -0300

    feat: configurando cors

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,20 @@ import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan'
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.use(morgan('dev'))
+
+  app.setGlobalPrefix('api')
+
+  app.enableCors(CORS)
 
   const configService = app.get(ConfigService)
 
   await app.listen(configService.get('PORT'));  
+  console.log(`Application runing on: ${await app.getUrl()}`)
 }
 bootstrap();
