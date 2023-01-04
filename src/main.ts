0a98cd7528043aed3be55e039ed3bac627633559
Lautaro Decima <decimalautaro@gmail.com>
2023-01-04 10:16:52 -0300

    feat: agregando morgan

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@nestjs/config/dist';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as morgan from 'morgan'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.use(morgan('dev'))
 
   const configService = app.get(ConfigService)
 
