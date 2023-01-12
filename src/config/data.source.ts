45e428b15451439775a13f8d23b6e6175d755643
Lautaro Decima <decimalautaro@gmail.com>
2023-01-12 12:56:49 -0300

    feat: configurando conexiones con la bd

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,13 +1,20 @@
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { DataSource, DataSourceOptions } from "typeorm";
 import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
+ConfigModule.forRoot({
+    envFilePath: `.${process.env.NODE_ENV}.env`,
+})
+
+const configService = new ConfigService()
+
 export const DataSourceConfig: DataSourceOptions = {
     type: 'postgres',
-    host: '',
-    port: 0,
-    username: '',
-    password: '',
-    database: '',
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
     entities: [__dirname + '/../**/**/*.entity{.ts,.js}'], 
     migrations: [__dirname + '/../../migrations/*.entity{.ts,.js}'],
     synchronize: false,
