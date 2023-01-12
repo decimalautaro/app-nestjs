3eb68dfb1358b970bf49e86c06c5c35c22691567
Lautaro Decima <decimalautaro@gmail.com>
2023-01-12 15:24:54 -0300

    feat: agregando relacion n a n a entidad projects

--- src/projects/entities/projects.entity.ts
+++ src/projects/entities/projects.entity.ts
@@ -1,5 +1,7 @@
+import { Column, Entity, OneToMany } from "typeorm";
+import { BaseEntity } from "src/config/base.entity";
 import { IProject } from "src/interfaces/project.interface";
-import { BaseEntity, Column, Entity } from "typeorm";
+import { UserProjectsEntity } from "src/users/entities/usersProjects.entity";
 
 @Entity({name: 'projects'})
 export class ProjectEntity  extends BaseEntity implements IProject{
@@ -8,5 +10,8 @@ export class ProjectEntity  extends BaseEntity implements IProject{
 
     @Column()
     description: string;
+
+    @OneToMany(()=> UserProjectsEntity, (usersProject)=> usersProject.project)
+    usersIncludes: UserProjectsEntity[]
 
 }
