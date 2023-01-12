bdf501ffd020f2c581f67874f4756950a3dee25e
Lautaro Decima <decimalautaro@gmail.com>
2023-01-12 12:58:59 -0300

    feat: importando modulos en app.modules.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule} from '@nestjs/typeorm'
+import { DataSourceConfig } from './config/data.source';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
@@ -8,7 +11,9 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
     }),
-    UsersModule
+    TypeOrmModule.forRoot({...DataSourceConfig}),
+    UsersModule,
+    ProjectsModule
   ],
 
 })
