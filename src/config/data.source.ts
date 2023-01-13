b756d8d1f652f73b461ebf67f2db81d706c8cf59
Lautaro Decima <decimalautaro@gmail.com>
2023-01-12 22:17:28 -0300

    fix:arreglando error en migratios

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -16,7 +16,7 @@ export const DataSourceConfig: DataSourceOptions = {
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_NAME'),
     entities: [__dirname + '/../**/**/*.entity{.ts,.js}'], 
-    migrations: [__dirname + '/../../migrations/*.entity{.ts,.js}'],
+    migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
     synchronize: false,
     migrationsRun: true,
     logging: false,
